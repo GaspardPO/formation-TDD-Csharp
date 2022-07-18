c3cb477f85c24d82fa9ac9ff6030409a09f2041e
Catherine Pereira <catherine.pereira@cdbdx.biz>
2022-07-18 14:21:36 +0200

    Refacto Compute

--- Yahtzee/Yahtzee/CombinationCalculator.cs
+++ Yahtzee/Yahtzee/CombinationCalculator.cs
@@ -9,10 +9,15 @@ namespace Yahtzee
         private const int One = 1;
         private const int Two = 2;
         private const int Three = 3;
-        public int ComputeOnes(int[] dices) => dices.Where(i => i== One).Sum();
 
-        public int ComputeTwo(int[] dices) => dices.Where(i => i== Two).Sum();
+        public int ComputeOnes(int[] dices) => Compute(dices, One);
+        
+        public int ComputeTwo(int[] dices) => Compute(dices, Two);
 
-        public int ComputeThree(int[] dices) => dices.Where(i => i== Three).Sum();
+        public int ComputeThree(int[] dices) => Compute(dices, Three);
+        private static int Compute(int[] dices, int value)
+        {
+            return dices.Where(i => i== value).Sum();
+        }
     }
 }
