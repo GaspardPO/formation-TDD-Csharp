801ee455d6aa75c69a969a3d6fb8212224361a6b
Chloé Belguermi <chloe.belguermi@cdiscount.com>
2022-07-18 16:10:34 +0200

    Adds GetTotal implementation

--- Yahtzee/Tests/UpperSectionShould.cs
+++ Yahtzee/Tests/UpperSectionShould.cs
@@ -8,25 +8,96 @@ namespace Tests
         [Fact]
         public void Return_0_when_all_combinations_have_no_points()
         {
-            var combinaison1 = new Combination(new[] { 2, 3, 4, 5, 2 }, 1);
-            var combinaison2 = new Combination(new[] { 1, 3, 4, 5, 1 }, 2);
-            var combinaison3 = new Combination(new[] { 1, 2, 4, 5, 1 }, 3);
-            var combinaison4 = new Combination(new[] { 1, 2, 1, 5, 1 }, 4);
-            var combinaison5 = new Combination(new[] { 1, 2, 4, 1, 1 }, 5);
-            var combinaison6 = new Combination(new[] { 1, 2, 4, 1, 1 }, 6);
-
+            var sumOfOnesIsZero = new Combination(new[] { 2, 3, 4, 5, 2 }, 1);
+            var sumOfTwosIsZero = new Combination(new[] { 1, 3, 4, 5, 1 }, 2);
+            var sumOfThreesIsZero = new Combination(new[] { 1, 2, 4, 5, 1 }, 3);
+            var sumOfFoursIsZero = new Combination(new[] { 1, 2, 1, 5, 1 }, 4);
+            var sumOfFivesIsZero = new Combination(new[] { 1, 2, 4, 1, 1 }, 5);
+            var sumOfSixIsZero = new Combination(new[] { 1, 2, 4, 1, 1 }, 6);
+            
+            
             var upperSection = new UpperSection(
-                combinaison1,
-                combinaison2,
-                combinaison3,
-                combinaison4,
-                combinaison5,
-                combinaison6);
+                sumOfOnesIsZero,
+                sumOfTwosIsZero,
+                sumOfThreesIsZero,
+                sumOfFoursIsZero,
+                sumOfFivesIsZero,
+                sumOfSixIsZero);
 
             var actual = upperSection.GetTotal();
 
-            //On a les 6 combinaisons à 0
             Assert.Equal(0, actual);
+        }
+        
+        [Fact]
+        public void Return_X_when_ones_combination_has_points()
+        {
+            var sumOfOnesIsTwo = new Combination(new[] { 1, 3, 1, 5, 2 }, 1);
+            var sumOfTwosIsZero = new Combination(new[] { 1, 3, 4, 5, 1 }, 2);
+            var sumOfThreesIsZero = new Combination(new[] { 1, 2, 4, 5, 1 }, 3);
+            var sumOfFoursIsZero = new Combination(new[] { 1, 2, 1, 5, 1 }, 4);
+            var sumOfFivesIsZero = new Combination(new[] { 1, 2, 4, 1, 1 }, 5);
+            var sumOfSixIsZero = new Combination(new[] { 1, 2, 4, 1, 1 }, 6);
+            
+            
+            var upperSection = new UpperSection(
+                sumOfOnesIsTwo,
+                sumOfTwosIsZero,
+                sumOfThreesIsZero,
+                sumOfFoursIsZero,
+                sumOfFivesIsZero,
+                sumOfSixIsZero);
+
+            var actual = upperSection.GetTotal();
+
+            Assert.Equal(2, actual);
+        }
+        
+        [Fact]
+        public void Return_X_when_two_combinations_have_points()
+        {
+            var sumOfOnesIsTwo = new Combination(new[] { 1, 3, 1, 5, 2 }, 1);
+            var sumOfTwosIsTwo = new Combination(new[] { 2, 3, 4, 5, 1 }, 2);
+            var sumOfThreesIsZero = new Combination(new[] { 1, 2, 4, 5, 1 }, 3);
+            var sumOfFoursIsZero = new Combination(new[] { 1, 2, 1, 5, 1 }, 4);
+            var sumOfFivesIsZero = new Combination(new[] { 1, 2, 4, 1, 1 }, 5);
+            var sumOfSixIsZero = new Combination(new[] { 1, 2, 4, 1, 1 }, 6);
+            
+            
+            var upperSection = new UpperSection(
+                sumOfOnesIsTwo,
+                sumOfTwosIsTwo,
+                sumOfThreesIsZero,
+                sumOfFoursIsZero,
+                sumOfFivesIsZero,
+                sumOfSixIsZero);
+
+            var actual = upperSection.GetTotal();
+
+            Assert.Equal(4, actual);
+        }
+        
+        [Fact]
+        public void Return_X_when_all_combinations_have_points()
+        {
+            var sumOfOnesIsTwo = new Combination(new[] { 1, 3, 1, 5, 2 }, 1);
+            var sumOfTwosIsTwo = new Combination(new[] { 2, 3, 4, 5, 1 }, 2);
+            var sumOfThreesIsSix = new Combination(new[] { 1, 2, 3, 3, 1 }, 3);
+            var sumOfFoursIsFour = new Combination(new[] { 1, 2, 1, 4, 1 }, 4);
+            var sumOfFivesIsFive = new Combination(new[] { 1, 2, 4, 1, 5 }, 5);
+            var sumOfSixIsSix = new Combination(new[] { 6, 2, 4, 1, 1 }, 6);
+
+            var upperSection = new UpperSection(
+                sumOfOnesIsTwo,
+                sumOfTwosIsTwo,
+                sumOfThreesIsSix,
+                sumOfFoursIsFour,
+                sumOfFivesIsFive,
+                sumOfSixIsSix);
+
+            var actual = upperSection.GetTotal();
+
+            Assert.Equal(25, actual);
         }
     }
 }
--- Yahtzee/Yahtzee/UpperSection.cs
+++ Yahtzee/Yahtzee/UpperSection.cs
@@ -4,14 +4,27 @@ namespace Yahtzee
 {
     public sealed class UpperSection
     {
+        private readonly Combination _combinaison1;
+        private readonly Combination _combinaison2;
+        private readonly Combination _combinaison3;
+        private readonly Combination _combinaison4;
+        private readonly Combination _combinaison5;
+        private readonly Combination _combinaison6;
+
         public UpperSection(Combination combinaison1, Combination combinaison2, Combination combinaison3, Combination combinaison4, Combination combinaison5, Combination combinaison6)
-        { 
-            
+        {
+            _combinaison1 = combinaison1;
+            _combinaison2 = combinaison2;
+            _combinaison3 = combinaison3;
+            _combinaison4 = combinaison4;
+            _combinaison5 = combinaison5;
+            _combinaison6 = combinaison6;
         }
 
         public int GetTotal()
         {
-            return 0;
+            return _combinaison1.GetValue() + _combinaison2.GetValue() + _combinaison3.GetValue() + _combinaison4.GetValue()
+                + _combinaison5.GetValue() + _combinaison6.GetValue();
         }
     }
 }
