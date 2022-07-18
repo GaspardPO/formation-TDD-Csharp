610e382ec57db230354a34cc515bb3b1e93b0f25
Amal Ben Sik Ali <amal.bensikali@cdiscount.com>
2022-07-18 15:36:27 +0200

    add combination test

---
+++ Yahtzee/Tests/CombinationShould.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using Yahtzee;
+
+namespace Tests
+{
+
+    public class CombinationShould
+    {
+
+        [Theory]
+        [InlineData(new[] { 2, 3, 4, 5, 1 }, new[] { 1, 2, 3, 4, 5, 0 })]
+        [InlineData(new[] { 1, 3, 5, 5, 1 }, new[] { 2, 0, 3, 0, 10, 0 })]
+        [InlineData(new[] { 3, 3, 3, 3, 3 }, new[] { 0, 0, 15, 0, 0, 0 })]
+        public void return_combination_dice(int[] dices, int[] expected)
+        {
+            int[] actual = new int[6];
+
+            actual[0] = new Combination(dices, 1).GetValue();
+            actual[1] = new Combination(dices, 2).GetValue();
+            actual[2] = new Combination(dices, 3).GetValue();
+            actual[3] = new Combination(dices, 4).GetValue();
+            actual[4] = new Combination(dices, 5).GetValue();
+            actual[5] = new Combination(dices, 6).GetValue();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Yahtzee/Yahtzee/Combination.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace Tests
+{
+    public class Combination
+    {
+        private int[] dices;
+        private int value;
+
+        public Combination(int[] dices, int value)
+        {
+            this.dices = dices;
+            this.value = value;
+        }
+
+        public int GetValue()
+        {
+            return dices.Where(i => i == value).Sum();
+        }
+
+
+    }
+}
