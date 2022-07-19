0fc492e36f8564606e266e392650ad82211b0765
Rémy Robert <45172609+RemyRobert@users.noreply.github.com>
2022-07-19 16:18:07 +0200

    add straight of 5

---
+++ Yahtzee/Tests/StraightShould.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using Yahtzee;
+
+namespace Tests
+{
+    public sealed class StraightShould
+    {
+        
+        [Theory]
+        [InlineData(new[] { 2, 1, 3, 4, 5 })]
+        [InlineData(new[] { 1, 2, 3, 4, 1 })]
+        [InlineData(new[] { 1, 3, 4, 5, 6 })]
+        [InlineData(new[] { 5, 2, 3, 4, 5 })]
+        public void Return_30_if_small_straight_exits(int[] round)
+        {
+            var smallStraight = new Straight(round,true);
+            var value = smallStraight.GetValue();
+            Assert.Equal(30,value);
+        }
+        
+        [Theory]
+        [InlineData(new[] { 1, 1, 1, 1, 1 })]
+        [InlineData(new[] { 1, 2, 1, 2, 1 })]
+        [InlineData(new[] { 1, 2, 1, 4, 5 })]
+        public void Return_0_if_small_straight_doesnt_exits(int[] round)
+        {
+            var smallStraight = new Straight(round,true);
+            var value = smallStraight.GetValue();
+            Assert.Equal(0,value);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1,2,3,4,5 })]
+        [InlineData(new[] { 6,3,2,4,5 })]
+        public void Return_40_if_large_straight_exits(int[] round)
+        {
+            var largeStraight = new Straight(round,false);
+            var value = largeStraight.GetValue();
+            Assert.Equal(40, value);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 6 })]
+        [InlineData(new[] { 2, 3, 4, 6, 1 })]
+        [InlineData(new[] { 6, 5 , 4, 3 ,1 })]
+        public void Return_0_if_large_straight_doesnt_exits(int[] round)
+        {
+            var largeStraight = new Straight(round,false);
+            var value = largeStraight.GetValue();
+            Assert.Equal(0, value);
+        }
+
+
+    }
+}
---
+++ Yahtzee/Yahtzee/Straight.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Yahtzee
+{
+    
+    public class Straight
+    {
+        private const int Four = 4;
+        private const int Five = 5;
+        private readonly int[] _round;
+        private readonly bool _isSmall;
+        private static readonly IEnumerable<IEnumerable<int>> _possibleSmallStraights = 
+            new[] { 
+                new[] { 1, 2, 3, 4 }, 
+                new[] { 2, 3, 4, 5 }, 
+                new[] { 3, 4, 5, 6 } 
+            };
+
+        private static readonly IEnumerable<IEnumerable<int>> _possibleLargeStraights = 
+            new[] { 
+                new[] { 1, 2, 3, 4, 5 }, 
+                new[] { 2, 3, 4, 5, 6 } 
+            };
+
+        public Straight(int[] round, bool isSmall)
+        {
+            _round = round;
+            _isSmall = isSmall;
+        }
+
+        public int GetValue()
+        {
+            if (!_isSmall)
+            {
+                return
+                    _possibleLargeStraights.Any(l => l.Intersect(_round).Count() == Five)
+                    ? 40 : 0;
+            }
+            else
+            {
+                return
+                    _possibleSmallStraights.Any(l => l.Intersect(_round).Count() == Four)
+                    ? 30 : 0;
+            }
+        }
+    }
+}
