546a6cf868889198f0adbc05238db77adb1aaa6f
Amal Ben Sik Ali <amal.bensikali@cdiscount.com>
2022-07-19 14:52:11 +0200

    add full house combination

---
+++ Yahtzee/Tests/FullHouseShould.cs
@@ -0,0 +1,41 @@
+using Xunit;
+using Yahtzee;
+
+namespace Tests
+{
+    public class FullHouseShould
+    {
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 })]
+        [InlineData(new[] { 2, 2, 3, 4, 5 })]
+        [InlineData(new[] { 1, 2, 3, 6, 5 })]
+        [InlineData(new[] { 1, 1, 2, 2, 5 })]
+        [InlineData(new[] { 1, 1, 1, 1, 2 })]
+        public void Return_0_when_Numbers_AreDifferentThenTwoAndThree(int[] round)
+        {
+            var fullHouseKo = new FullHouseCombination(round);
+
+            var actual = fullHouseKo.GetValue();
+
+            Assert.Equal(0, actual);
+        }
+
+
+        [Theory]
+        [InlineData(new[] { 1, 1, 1, 2, 2 })]
+      
+        public void Return_sum_when_have_threeAndTwo_identical_dices(int[] round)
+        {
+            var fullHouseOK = new FullHouseCombination(round);
+
+            var actual = fullHouseOK.GetValue();
+
+            Assert.Equal(25, actual);
+        }
+       
+            
+    }
+
+
+}
---
+++ Yahtzee/Yahtzee/FullHouseCombination.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace Yahtzee
+{
+    public class FullHouseCombination
+    {
+        private int[] round;
+
+        public FullHouseCombination(int[] round)
+        {
+            this.round = round;
+
+        }
+
+        public int GetValue()
+        {
+            var groups = round.ToList()
+                            .GroupBy(diceValue => diceValue);
+            return
+               groups.Any(group => group.Count() == 3)
+               &&
+               groups.Any(group => group.Count() == 2)
+                ? 25
+                : 0;
+        }
+    }
+}
