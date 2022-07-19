79ffae6253ec3f1396271067356384a07c976b39
Catherine Pereira <catherine.pereira@cdbdx.biz>
2022-07-19 17:01:35 +0200

    Add YahtzeeCombination

---
+++ Yahtzee/Tests/YahtzeeShould.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using Yahtzee;
+
+namespace Tests
+{
+    public class YahtzeeShould
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 5 })]
+        [InlineData(new[] { 1, 1, 1, 4, 5 })]
+        public void Return_0_if_different_values(int[] round)
+        {
+            var yahtzeeKo = new YahtzeeCombination(round);
+            var value = yahtzeeKo.GetValue();
+            Assert.Equal(0,value);
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 1, 1, 1, 1 })]
+        [InlineData(new[] { 5, 5, 5, 5, 5 })]
+        public void Return_50_if_same_values(int[] round)
+        {
+            var yahtzeeKo = new YahtzeeCombination(round);
+            var value = yahtzeeKo.GetValue();
+            Assert.Equal(50, value);
+        }
+    }
+}
---
+++ Yahtzee/Yahtzee/YahtzeeCombination.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Yahtzee
+{
+    public class YahtzeeCombination
+    {
+        private readonly int[] _round;
+
+        public YahtzeeCombination(int[] round)
+        {
+            _round = round;
+        }
+        public int GetValue()
+        {
+            return _round
+                .GroupBy(diceValue => diceValue)
+                .Count() == 1
+                ? 50
+                : 0;
+        }
+    }
+}
